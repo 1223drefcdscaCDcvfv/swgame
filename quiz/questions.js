3281e56f337c5067a3b5d1a83a99faa6ebc15e4d
1223drefcdscaCDcvfv <93698234+1223drefcdscaCDcvfv@users.noreply.github.com>
2021-11-04 20:19:30 -0700

    Add files via upload

---
+++ quiz/questions.js
@@ -0,0 +1,68 @@
+let questions = [
+{
+    numb: 1,
+   question:  "What is the name of this game ?",
+   answer:"Game Maniac",
+   options: [
+       "Game Maniac",
+       "Adventureio",
+       "shooter",
+       "Long-passer",
+   ]
+},
+{
+    numb: 2,
+   question:  "When was this game established?",
+   answer:"10/3/2021",
+   options: [
+       "11/3/2021",
+       "10/3/2021",
+       "12/3/2021",
+       "10/3/2022",
+   ]
+},
+{
+    numb: 3,
+   question:  "Who is the Owner of this game?",
+   answer:"Swastik Paudel",
+   options: [
+       "Swastik Pokherel",
+       "Samar Paudel ",
+       "Swastik Paudel",
+       "Swasatik Paudel",
+   ]
+},
+{
+    numb: 4,
+   question:  "What is better for your health ?",
+   answer:"Water",
+   options: [
+       "Water",
+       "Food",
+       "Regular Exercise",
+       "Balance diet",
+   ]
+},
+{
+    numb: 5,
+   question:  "What is your favorate game?",
+   answer:"Go-to-where",
+   options: [
+       "Go-to-where",
+       "Candy crush",
+       "Pupg",
+       "Free-Fire",
+   ]
+},
+{
+    numb: 6,
+   question:  "What do you think about this game?",
+   answer:"Excellent",
+   options: [
+       "Nice",
+       "Worse",
+       "Excellent",
+       "Acceptable",
+   ]
+},
+];
